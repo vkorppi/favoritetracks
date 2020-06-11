70782b0339bd29d991b34f4ebdd009ed649b473f
Ville <korppi.villekorppi.ville@gmail.com>
2020-06-11 12:31:19 +0300

    tehtävä kappaleiden haku: backend grapql aloitettu

---
+++ backend/src/graphql/queries.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/semi */
+
+import { ApolloServer, gql,UserInputError,AuthenticationError } from 'apollo-server';
+
+const typeDefs = gql`
+
+type Query {
+ 
+  }
+
+`
