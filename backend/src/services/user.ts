25316b74fe6b87ca41eb1898cd75e31aa96d175c
Ville <korppi.villekorppi.ville@gmail.com>
2020-06-22 10:31:15 +0300

    tehtävä käyttäjienhallinta: backend services jatketaan

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,11 +1,14 @@
 
 
 import bcrypt from 'bcryptjs';
-// import typeparsers from '../utils/typeparsers'
+ import typeparsers from '../utils/typeparsers'
 
 const create = (username:string,password:string, firstname: String,lastname: String): void  => {
 
-  
+    typeparsers.parseString(username,'username: username was not a string');
+    typeparsers.parseString(password);
+    typeparsers.parseString(firstname);
+    typeparsers.parseString(lastname);
 
     const hashedVal =  bcrypt.hashSync(password,10);
 
