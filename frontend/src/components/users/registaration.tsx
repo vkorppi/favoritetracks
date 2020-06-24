3c887c22e9c1a0a93234e87e6c62e9a10c8aba6a
Ville <korppi.villekorppi.ville@gmail.com>
2020-06-24 10:31:52 +0300

    tehtävä käyttäjienhallinta: frontend jatketaan

--- frontend/src/components/users/registaration.tsx
+++ frontend/src/components/users/registaration.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { RegistarationType } from "../../type";
 
 const Registaration: React.FC<RegistarationType> = () => {
@@ -8,43 +8,48 @@ const Registaration: React.FC<RegistarationType> = () => {
 
     return (
         <div >
-            <div className="container">
-                <form >
-                    <div className="form-group row">
-                        <div className="col-xs-2">
-                            <div>
-                                <input className="form-control" id="ex1" type="text" /></div>
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-xs-2">
-                            <div>
-                                <input className="form-control" id="ex1" type="text" /></div>
-                        </div>
-                    </div>
+            <Card>
+                <Card.Header>Registaration</Card.Header>
+                <Card.Body>
+                    <div className="container">
+                        <form >
+                            <div className="form-group row">
+                                <div className="col-xs-2">
+                                    <div>
+                                        <input className="form-control" id="ex1" type="text" /></div>
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <div className="col-xs-2">
+                                    <div>
+                                        <input className="form-control" id="ex1" type="text" /></div>
+                                </div>
+                            </div>
 
-                    <div className="form-group row">
-                        <div className="col-xs-2">
-                            <div>
-                                <input className="form-control" id="ex1" type="text" /></div>
-                        </div>
-                    </div>
+                            <div className="form-group row">
+                                <div className="col-xs-2">
+                                    <div>
+                                        <input className="form-control" id="ex1" type="text" /></div>
+                                </div>
+                            </div>
 
-                    <div className="form-group row">
-                        <div className="col-xs-2">
-                            <div>
-                                <input className="form-control" id="ex1" type="text" /></div>
-                        </div>
-                    </div>
+                            <div className="form-group row">
+                                <div className="col-xs-2">
+                                    <div>
+                                        <input className="form-control" id="ex1" type="text" /></div>
+                                </div>
+                            </div>
 
-                    <div className="form-group row">
-                        <div className="col-xs-2">
-                            <Button type="submit" variant="primary" >Register </Button>
-                        </div>
+                            <div className="form-group row">
+                                <div className="col-xs-2">
+                                    <Button type="submit" variant="primary" >Register </Button>
+                                </div>
+                            </div>
+                        </form>
+
                     </div>
-                </form>
- 
-            </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 
