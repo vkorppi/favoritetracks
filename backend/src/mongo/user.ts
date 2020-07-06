00f734439e5ce1e0c0d3a71bef8a8e20b171dbf3
Ville <korppi.villekorppi.ville@gmail.com>
2020-07-06 16:38:12 +0300

    Siirto spotify:n: tehdään frontendiä tukevia muutoksia backend:n aloitettu

--- backend/src/mongo/user.ts
+++ backend/src/mongo/user.ts
@@ -10,7 +10,8 @@ const userSchema: Schema = new Schema({
   birthdate: { type: String },
   email: { type: String },
   address: { type: String },
-  favorites: { type: String }
+  favorites: { type: String },
+  playlist: { type: String }
 });
 
 
